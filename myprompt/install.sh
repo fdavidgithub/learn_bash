350842e6b7dc902d9965973a206d963f5458a8a3
francis DAVID <contato@fdavid.com.br>
2022-02-25 06:58:47 -0300

    upd: Include CR

--- myprompt/install.sh
+++ myprompt/install.sh
@@ -6,7 +6,7 @@ export WORK_DIR=$(pwd)
 echo "# My Settings..." >> $HOME/$CONFIG_FILE
 echo "source $WORK_DIR/git-prompt.sh" >> $HOME/$CONFIG_FILE
 
-export PS1='\u@\h\[\033[01;34m\] \w\[\033[0;32m\]$(__git_ps1 " (%s)")\n\[\033[01;34m\]$\[\033[00m\] '
+export PS1='\n\u@\h\[\033[01;34m\] \w\[\033[0;32m\]$(__git_ps1 " (%s)")\n\[\033[01;34m\]$\[\033[00m\] '
 
 echo "PS1='$PS1'" >> $HOME/$CONFIG_FILE 
 
